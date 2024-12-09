bb986db5308e5c6db748d33d5b697042e0f2913a
tahaali73 <121288774+tahaali73@users.noreply.github.com>
2024-12-09 04:10:08 -0800

    iss_locator_programm

--- main.py
+++ main.py
@@ -1,110 +1,51 @@
-from tkinter import *
-from tkinter import messagebox
-from random import choice, randint, shuffle
-import pyperclip
-import json
+import requests
+from datetime import datetime
+import time
+import smtplib
+my_email = "soomrotaha07@gmail.com"
+password = "uqvabgefrqbpswkf"
+MY_LAT = 33.7215 # Your latitude
+MY_LONG = 73.0433 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+print(iss_latitude)
+print(MY_LAT)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now().hour
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+while True:
+   time.sleep(60)
+   if ((MY_LAT - 5 <= iss_latitude <= MY_LAT + 5) and (MY_LONG - 5 <= iss_longitude <= MY_LONG + 5)) and ((sunset + 3) <= time_now <= (sunrise + 2)):
+      
+     with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="tsoomro76@yahoo.com",
+                           msg=f"Subject:ISS LOCATION\n\nISS is above you\nlat:{iss_latitude}\nlong:{iss_longitude}\ntime:{time_now}.")
+   
 
 
-# ---------------------------- Search Functtion ------------------------------- #
 
-def Search_password():
-    website_name = website_entry.get()
-
-    with open('data.json','r') as search_data:
-        data = json.load(search_data)
-    if website_name in data:
-        messagebox.showinfo("Record",f"website: {website_name}\nemail: {data[website_name]['email']}\npassword: {data[website_name]['password']}")
-    else: messagebox.showinfo("Record","Record not found")
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-#Password Generator Project
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-    password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-
-    password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
-
-    password = "".join(password_list)
-    password_entry.insert(0, password)
-    pyperclip.copy(password)
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-
-    new_data = {
-                website : {
-                    'email': email,
-                    'password':password
-                    }
-                 }
-
-    if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        try:    
-            with open("data.json", "r") as data_file:
-                data = json.load(data_file)
-            
-        except FileNotFoundError:
-            with open('data.json','w') as data_file:
-                json.dump(new_data,data_file, indent=4)
-
-        else:
-            data.update(new_data)
-       
-            with open('data.json', 'w') as data_file:
-                json.dump(data,data_file,indent=4)
-
-        finally:
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-
-
-# ---------------------------- UI SETUP ------------------------------- #
-
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
-
-#Labels
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
-
-#Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "diytech960@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
-
-# Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
-search_button = Button(text="Search", width=15,command=Search_password)
-search_button.grid(row=1,column=3)
-
-window.mainloop()
